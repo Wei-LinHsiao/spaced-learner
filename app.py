fb0fad14eabd79b625f41c92945837c0122ee4fe
Wei-Lin Hsiao <36719288+Wei-LinHsiao@users.noreply.github.com>
2019-07-26 14:16:46 -0500

    Update tester to use Leitner boxes

--- app.py
+++ app.py
@@ -17,44 +17,27 @@ if debug:
     current_box.create_entry("Six", "")
     current_box.create_entry("Seven", "")
 
-
 # Updates all entries, given the status of their info.
 def start_day():
     ## This code is at the start of a "day"; sort and figure things out
     # Look at current_entries and modify the status of all_entries by status
-    for i in range(0, len(current_entries)):
-        status = current_entries[i]["status"]
-
+    for entry in current_box.get_all_cards():
         # Red
-        if status == 2:
+        if entry.status == 2:
             # Lower priority value; want to see soon
-            all_entries[i]["priority"] = all_entries[i]["priority"]  * 0.25 - 5
+            current_box.downgrade_entry(entry)
         # Grey
-        elif status == 3:
-            # Add to priority: Can see soon
-            all_entries[i]["priority"] = all_entries[i]["priority"]  * 0.25 + 2
+        elif entry.status == 3:
+            # Do nothing
+            entry.status = 3
         # Green
-        elif status == 4:
-            # Add to priority: Can see soon
-            all_entries[i]["priority"] = all_entries[i]["priority"]  * 0.25 + 5
+        elif entry.status == 4:
+            current_box.upgrade_entry(entry)
+        entry.status = 0
 
-    # Sort entires by priority before rendering
-    all_entries.sort(key=lambda x: x["priority"])
-
-    # Clear current entries, and get the top three overall entries
-    # Need to check for case where < 3 entries
-    # Change their statuses to 1 to indicate usage
-    del current_entries[0:len(current_entries)]
-    current_entries.extend(all_entries[0:3])
-
-    for i in range(0, 3):
-        current_entries[i]["status"] = 1
-
+    # Advance the day.
+    current_box.cur_day += 1
 # General Functions
-
-## Route Functions: API endpoints.
-@app.route('/entry/tes', methods=["GET", "POST"])
-
 
 ## Route Functions: Render main pages.
 @app.route('/tester', methods=["GET", "POST"])
@@ -65,37 +48,33 @@ def tester():
         if "submit" in request.form:
             start_day()
         else:
+            print(request.json)
             # Get color, and modify accordingly
             id = int(request.json["e_id"])
+            entry = current_box.get_entry(id)
             color = request.json["color"]
 
-            # Change the priority of our current entry
-            # Get id of current entry
-            for i in range(0, len(current_entries)):
-                if current_entries[i]["e_id"] == id:
-                    id_idx = i
-
-            # Update color and status values
-            # Don't change priority if status is not 1
-            # Return value to normal if status is already that color
-            cur_status = current_entries[id_idx]["status"]
+            # # Update color and status values
+            # # Don't change priority if status is not 1
+            # # Return value to normal if status is already that color
+            cur_status = entry.status
             if color == "red":
                 if cur_status != 2:
-                    current_entries[id_idx]["status"] = 2
+                    entry.status = 2
                 else:
-                    current_entries[id_idx]["status"] = 1
+                    entry.status = 1
             if color == "grey":
                 if cur_status != 3:
-                    current_entries[id_idx]["status"] = 3
+                    entry.status = 3
                 else:
-                    current_entries[id_idx]["status"] = 1
+                    entry.status = 1
             if color == "green":
                 if cur_status != 4:
-                    current_entries[id_idx]["status"] = 4
+                    entry.status = 4
                 else:
-                    current_entries[id_idx]["status"] = 1
+                    entry.status = 1
 
-    return render_template('tester.html', entries = current_entries)
+    return render_template('tester.html', entries = current_box.get_current_cards())
 
 # Run this code before Flask starts
 start_day()
